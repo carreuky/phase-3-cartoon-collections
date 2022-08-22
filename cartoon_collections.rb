4ffddaa818e4086183b91a3471932fad5b705f4c
carreuky <carreuky@gmail.com>
2022-08-22 14:28:42 +0300

    commit tree 9b4d36b2774f458cbf05cd0f97e6c6354f139150

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,15 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index do |elem,index|
+    puts "#{index.to_i+1}. #{elem}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+array.map do |elem|
+  elem.capitalize + "!"
+end
 end
 
 def long_planeteer_calls# code an argument here
@@ -13,4 +19,16 @@ end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+end
+
+def long_planeteer_calls(array)
+array.any? do |elem|
+elem.length > 4
+end
+end
+
+def find_the_cheese (array)
+array.find do |elem|
+elem=="cheddar"
+end
 end
